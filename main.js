2f00e9bc5e65453efab90406bd536c2305b289a5
bp277 <bosko277@gmail.com>
2022-05-01 18:27:44 +0200

    added reset button

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let userChoice = 16
 const gridContainer = document.querySelector('#grid-container');
 let cellNum = document.querySelector('.cell-number')
-
-for (let i = 0; i <= userChoice * userChoice; i++) {
-    let gridItem = document.createElement('div');
-    gridContainer.style.setProperty('--grid-size', userChoice);
-    gridContainer.appendChild(gridItem).className = 'grid-item';
-    gridItem.addEventListener ('mouseover', () => {
-        let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        gridItem.style.backgroundColor = '#' + randomColor
-    })  
+function makeRows(rows, cols) {
+    for (let i = 0; i < rows * cols; i++) {
+        let gridItem = document.createElement('div');
+        gridContainer.appendChild(gridItem).className = 'grid-item';
+        gridItem.addEventListener ('mouseover', () => {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            gridItem.style.backgroundColor = '#' + randomColor
+        })  
+    }    
 }
+makeRows(16, 16)
+
+cellNum.addEventListener('click', (size) => {
+    size = prompt('Enter the number of rows and columns.')
+    gridContainer.innerHTML = ''
+    gridContainer.style.setProperty('--grid-size', size);
+    makeRows(size, size)
+
+})
